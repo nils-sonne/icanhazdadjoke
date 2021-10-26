91008a0b86eba1a5aa6baf33ffeb9b10ea7bf9db
Nils-Sonne <U2407197@RTS-365.DK>
2021-10-26 09:26:31 +0200

    basic

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+
+// når der står require, så skal det installeres via npm install ( eksempel gulp-uglify)
+
+function buildStyles() {
+  return src('index.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTask() {
+  watch(['index.scss'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
---
+++ script.js
@@ -0,0 +1,20 @@
+// Variables
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById('jokeBtn');
+
+// Functions
+jokeBtn.addEventListener('click', function changeJoke() {
+    generateJoke();
+})
+
+
+function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+    fetch('https://icanhazdadjoke.com', config).then(response => response.json()).then((data) => {
+        jokeEl.innerHTML = data.joke
+    });
+}
